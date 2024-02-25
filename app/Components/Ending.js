54bd2d02850dbf63382357e958c667305dcbc056
SCHRLchch <3101847378a@gmail.com>
2024-02-25 22:24:36 +0100

    Linking to my agency

--- app/Components/Ending.js
+++ app/Components/Ending.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export const Ending = () => {
   return (
     <div className="bg-emerald-950 text-white  w-screen h-screen flex justify-center items-center ">
       <h1 className=" text-9xl">Start with us Now</h1>
+      <p>
+        Programmed by<Link href="https://www.lrhcs.ch/">LRHCS</Link>
+      </p>
     </div>
   );
 };
